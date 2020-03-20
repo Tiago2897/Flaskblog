91ae748c1ab6f756f94aa17f81ebeeaa11814a7a
Tiago <tiagogoncalves@gmail.com>
2020-03-20 22:53:38 +0000

    Primeiro Commit

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,18 @@
 from app import app
+from flask import render_template
+
 @app.route('/') 
 @app.route('/index') 
 def index(): 
-    return "Hello, World!"
+    return render_template("index.html")
+
+
+
+@app.route('/contacts')
+def contacts():
+    return render_template("contacts.html")
+
+@app.route ('/about')
+def about():
+    return render_template("about.html")
+
